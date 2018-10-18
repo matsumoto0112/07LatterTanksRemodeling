1ee3e5cb4406326f6c6b80fb4b7c5f4390cc8e43
ryoitym <ryo.takamy1222@gmail.com>
2018-10-18 10:28:06 +0900

    風船の実装

---
+++ 07LatterTanksRemodeling/Assets/Scripts/Ballon/Ballon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballon : MonoBehaviour {
+
+    private Rigidbody rigid;
+
+    private Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+        pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        pos.y = pos.y + 0.01f;
+        rigid.MovePosition(new Vector3(pos.x + Mathf.PingPong(Time.time, 2), pos.y, pos.z));
+    }
+}
