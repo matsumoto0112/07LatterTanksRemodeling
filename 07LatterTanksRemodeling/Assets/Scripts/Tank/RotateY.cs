7b45f43b1e378d089e953985f58932364bf73933
matsumoto <matsumoto.k017a1377@gmail.com>
2018-10-25 12:08:25 +0900

    統合

---
+++ 07LatterTanksRemodeling/Assets/Scripts/Tank/RotateY.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateY : MonoBehaviour
+{
+
+    [SerializeField]
+    private CameraChangeFirstThird cameraChangeFirstThird;
+    [SerializeField]
+    private float speed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cameraChangeFirstThird.mode == Person.Third) return;
+        float y = Input.GetAxisRaw("Horizontal1");
+        this.transform.Rotate(new Vector3(0, 1, 0), y * speed);
+    }
+}
